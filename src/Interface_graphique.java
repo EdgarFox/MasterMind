a285e2d47f611a26ef3f219613d8b4f190fef51c
Edgar Poujol <epoujo01@pcalb-mm0000.univ-jfc.fr>
2021-02-17 12:13:29 +0100

    Interface graph V3

--- src/Interface_graphique.java
+++ src/Interface_graphique.java
@@ -1,5 +1,11 @@
+import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.Panel;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
@@ -11,15 +17,40 @@ public class Interface_graphique extends Frame implements WindowListener {
 		this.setVisible(true);
 		this.setSize(500, 800);
 		this.addWindowListener(this);
-		Canvas CMM = new Canvas();
-		CMM.setSize(500, 800);
+		Canvas CMM = new Canvas() {
+			public void paint(Graphics g) {
+				g.drawString("pouet", 50, 50);
+			}
+		};
+		//CMM : Canvas MasterMind
+		CMM.setSize(500, 500);
+		this.add(CMM);
+		
+		Panel PMM = new Panel();
+		PMM.setBackground(Color.white);
+		PMM.setSize(500, 600);
+		
+		PMM.setLayout(new GridLayout(1,Modele.COULEURS.length));
+		
+		
+		this.setLayout(new BorderLayout());
+		this.add(PMM, BorderLayout.SOUTH);
+		
+		for (int i=0; i<Modele.COULEURS.length;i++) {
+			Button BMM = new Button();
+			PMM.add(BMM);
+			BMM.setBackground(Modele.COULEURS[i]); 
+			
+		}
+			
+		
+		
+		
+		
 	}
 	
 	public static void main (String[] args) {
-		
 		new Interface_graphique();
-		
-		
 	}
 
 	@Override
