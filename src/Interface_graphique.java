9c96b5d8a8c028b0aee05a83bd91565aba5b6708
Edgar Poujol <epoujo01@pcalb-mm0000.univ-jfc.fr>
2021-02-17 11:11:08 +0100

    Interface graph V2

--- src/Interface_graphique.java
+++ src/Interface_graphique.java
@@ -1,9 +1,67 @@
+import java.awt.Canvas;
+import java.awt.Frame;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-public class Interface_graphique {
+@SuppressWarnings("serial")
+public class Interface_graphique extends Frame implements WindowListener {
+	
+	public Interface_graphique() {
+		super();
+		this.setVisible(true);
+		this.setSize(500, 800);
+		this.addWindowListener(this);
+		Canvas CMM = new Canvas();
+		CMM.setSize(500, 800);
+	}
+	
+	public static void main (String[] args) {
+		
+		new Interface_graphique();
+		
+		
+	}
 
-	public static void main(String[] args) {
+	@Override
+	public void windowActivated(WindowEvent arg0) {
 		// TODO Auto-generated method stub
+		
+	}
 
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent arg0) {
+		System.exit(0);
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
