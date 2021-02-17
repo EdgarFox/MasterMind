2754b71bcacfb194c7252ac56f16cae740c3bf33
Theo Cavailles <tcavai01@pcalb-mm0000.univ-jfc.fr>
2021-02-17 10:45:19 +0100

    ajout des attributs du modele

--- src/Modele.java
+++ src/Modele.java
@@ -1,4 +1,20 @@
+import java.awt.Color;
 
 public class Modele {
-
+	
+	public Color[] COULEURS = {Color.YELLOW,
+						Color.GREEN,
+						Color.BLUE,
+						Color.MAGENTA,
+						Color.RED,
+						Color.ORANGE,
+						Color.WHITE,
+						Color.BLACK};
+	public int N_TENTATIVES = 10;
+	public int DIFFICULTE = 4;
+	public enum ETAT {EN_COURS, GAGNE, PERDU};
+	
+	public Modele() {
+		
+	}
 }
